5e9df4b7fe750520698c28cef132b2220c08b34e
Pere <pere.solaclaver@gmail.com>
2019-09-30 13:15:48 +0100

    Input form and data saving to the slices of state: form handler and click handler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 
+import TodoForm from './components/TodoComponents/TodoForm';
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+constructor(props) {
+  super(props);
+  this.state = {
+    todos: [],
+    formTodo: '',
+  };
+}
+
+changeEventHandler = event => {
+  this.setState({
+    formTodo: event.target.value,
+  })
+}
+
+clickEventHandler = event => {
+  this.setState(currentState => (
+    {todos: currentState.todos.concat(this.state.formTodo)}
+  ))
+}
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+
+        <TodoForm changeEventHandler={this.changeEventHandler} clickEventHandler={this.clickEventHandler} /> 
       </div>
     );
   }
